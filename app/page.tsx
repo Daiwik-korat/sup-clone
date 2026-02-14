8828d91368014228564b9b34941f2bcbbf4781a1
Daiwik-korat <daiwikk@techlusion.io>
2026-02-14 15:13:18 +0530

    Main

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,17 @@ import Hero from "../components/Hero";
 import FeatureHighlight from "../components/featureHighlights/featureHighlight";
 import HowitWorks from "../components/howItWorks/howItWorks";
 import ProductsSec from "../components/ProductsSec/ProductsSec";
-import Membership from "../components/membership/Membership"
+import Membership from "../components/membership/Membership";
 
 export default function Home() {
-
   return (
     <>
       <Navbar />
       <Hero />
       <FeatureHighlight />
       <HowitWorks />
-      <ProductsSec/>
-      <Membership/>
+      <ProductsSec />
+      {/* <Membership/> */}
     </>
   );
 }
